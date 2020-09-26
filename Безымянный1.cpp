1758dfcc918f0c0b50fbd7cf7abe15804cce91da
denis <denis0520dem@gmail.com>
2020-09-26 13:41:22 +0600

    talk

---
+++ Безымянный1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctime> 
+#include <cstdlib>
+using namespace std;
+int main () {
+	int a=-1;
+	int b=0;
+	int c=0;
+	cin >> c;
+	while (a<c)
+{
+	if (a%2) 
+
+{
+				b=b+1;}
+		a=a+1;
+}
+cout << b;
+return 0;
+}
+

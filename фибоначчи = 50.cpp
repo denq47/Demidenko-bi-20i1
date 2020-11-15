279c9823dd2f7da0a764d4a071b0b72506031981
denq47 <71919116+denq47@users.noreply.github.com>
2020-11-15 22:45:29 +0500

    Add files via upload

---
+++ фибоначчи = 50.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+unsigned long long Fib(int i)
+{
+	int a = 0;
+	if(i < 1) return 0;
+	if(i == 1) return 1;
+	return Fib(i-1) + Fib(i - 2);
+}
+int main()
+{
+	int i = 0;
+	while(i < 51)
+{
+	cout << Fib(i) << endl;
+	i++;
+}
+return 0;
+}
